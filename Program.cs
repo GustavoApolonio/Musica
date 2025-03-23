e989603b92b2d47e3ace0a99ce4580bf755b27ec
Gustavo Apolonio <gustavoapolinio123@gmail.com>
2025-03-23 12:41:56 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Diagnostics;
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine(@"
+ ____       _        _ _                       __  __                   
+|  _ \ __ _| |_ ___ | (_)_ __   ___     ___   |  \/  | __ _  __ _  ___  
+| |_) / _` | __/ _ \| | | '_ \ / _ \   / _ \  | |\/| |/ _` |/ _` |/ _ \ 
+|  __/ (_| | || (_) | | | | | | (_) | | (_) | | |  | | (_| | (_| | (_) |
+|_|   \__,_|\__\___/|_|_|_| |_|\___/   \___/  |_|  |_|\__,_|\__, |\___/ 
+                                                            |___/       ");
+Thread.Sleep(5000);
+Console.ResetColor();
+Console.WriteLine(@"
+                                        ___
+                                __  _/:::>__
+                               /:/_/::/ _/::>
+                             _/:(/:::\_/::/
+                            _):::::::::::::\
+                          _/::::::::::::::::\____
+                         /      \:::::::::/      \
+                        |  ::/\  ::::::::  / \::   |
+                        / ::/  \  ::::::  /   |:::/
+                       /:::|    \::::::::/    |:::\
+                      /::::|     \::::::/     |::::\
+                    ,------:      \::::/      :------,
+                   /   ___  \0    /    \ 0   / ___  \
+                  : ,-' ) `  `---'      `---'   ( `-,  :
+                  \_    \         '     `        \_  _/
+                    \____\                         \/
+                          \                  _______\________
+                           \              ,-'                )
+                            \           ,-    ,----------- _/
+
+                             \             ,-'      \\ ) _/
+                              (___________/__________\\ /
+                               :;;;\___________________)
+                        ______,:;;;;;;;;:______
+                      ,;;;;;;;;;;;;;;;;;;;;;;;;\_
+                     /;;;;;;;;;;;;;;;;;;;;;;;;;;;\_
+                    /;;;;;;__;;;; ;;;;;; ;;;;;;;;;;\");
+Thread.Sleep(5000);
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine(@"
+Contemplem o mago!
+Com seus poderes
+Incríveis poderes!");
+Thread.Sleep(7000);
+Console.WriteLine(@"
+Sob o olhar do necromante
+A escada prateada vai se erguer
+As pessoas maravilhadas
+Com seus olhos cheios de poder
+Comida fria vai esquentar
+Ao enfeitiçar!
+Balançando as suas mãos
+O cachorro quente explodirá!");
+Thread.Sleep(10000);
+Console.WriteLine(@"
+Na presença do grande mago
+O trânsito para de repente
+Pode atravessar a rua
+Com os carros parados na sua frente");
+Thread.Sleep(5000);
+Console.WriteLine(@"
+Na tv mudam-se os canais
+Sem que saia do sofá
+Sua varinha pega então
+Pra reclinar-se no ar");
+Thread.Sleep(10000);
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Pela luz fraca do sol negro do reino dos sonhos.
+O mago sobe as cataratas congeladas de Voldrini
+em busca de Celestia, a guardiã do poder infinito
+Quando de repente um terrível Garlon aparece
+e ataca com gelo, ávelmas o mago é implac
+O Garlon ruge e libera um vento muito sinistro,
+mas o mago é implacável."); //mago implavel
+Thread.Sleep(10000);
+Console.WriteLine(@"
+O Garlon invoca as pedras de Prophynia,
+mas o mago é implacável. 
+Invocando os poderes dos ancestrais,
+o mago conjura o fogo sagrado
+e lança seu feitiço na larva derretida de um Gort insaciável.
+(Obrigado, Gort!)"); // mago implacavel
+Thread.Sleep(5000);
+Console.WriteLine(@"
+Ahh, Celestia. Acho que você vai adorar isso
+O mago fica diante do precipício do poder definitivo,
+os portões se abrem para revelar...");
+Thread.Sleep(10000);
+Console.Clear();
+Console.ResetColor();
+Console.Write("Patolino: ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("hmm...");
+Thread.Sleep(2000);
+Console.WriteLine("Quanto tempo eu fiquei dormindo?");
+Thread.Sleep(3000);
+Console.ResetColor();
+Console.WriteLine("Pernalonga: 3 dias");
+Thread.Sleep(5000);
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"
+Faminto por causa da última missão
+O mago quer lanchar
+Traça o rumo do prazer
+Pro habitual lugar
+Ele é o mago
+O místico mago");
+Console.ResetColor();
